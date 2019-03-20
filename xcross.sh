829099c15377a08e2a98321c1c9db59979af0d0d
nirekin <nirekin@hotmail.com>
2019-03-20 13:54:16 +0100

    Update travis relatively to sub packages

--- xcross.sh
+++ xcross.sh
@@ -1,5 +1,5 @@
-docker run --rm -it -v "$PWD":/go/src/awesomegostars -w /go/src/awesomegostars dockercore/golang-cross:1.11.0 sh -c '
-    for GOOS in darwin linux windows; do
+docker run --rm -it -v "$PWD":/go/src/github.com/nirekin/awesomegostars -w /go/src/github.com/nirekin/awesomegostars dockercore/golang-cross:1.11.0 sh -c '
+	for GOOS in darwin linux windows; do
       for GOARCH in 386 amd64; do
         echo "Building $GOOS-$GOARCH"
         export GOOS=$GOOS
